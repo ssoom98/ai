33319db371fbb3f238a09e43891843e2ed5b07a1
ssoom98 <breathin9898@gmail.com>
2024-09-04 18:02:02 +0900

    0904

---
+++ source/01_java/ch09_inheritance/src/com/lec/ex5_super/Baby.java
@@ -0,0 +1,21 @@
+package com.lec.ex5_super;
+//super() : 부모 클래스의 생성자 함수
+//super.intro () : 부모 클래스의 intro()
+// private(name, character) public(intro(),getter&setter)
+public class Baby extends Person {
+
+	public Baby() {
+		//super(); //부모 클래스의 매개변수 없는 생성자 함수. super()생략할 경우 super() 자동 실행
+		System.out.println("매개변수 없는 Baby 생성자");
+	}
+	public Baby (String name, String character) {
+		super(name,character); // 부모클래스의 매개변수 있는 생성자 함수. 반드시 메소드 첫번째 줄에 호출
+		System.out.println("매개변수 있는 Baby 생성자");
+	}
+	@Override
+	public void intro() {
+		System.out.print("(◑ㅁ◐) 응애");
+		super.intro(); //super.은 어디에 있어도 된다.
+	}
+
+}
---
+++ source/01_java/ch09_inheritance/src/com/lec/ex6_final/Animal.java
@@ -0,0 +1,12 @@
+package com.lec.ex6_final;
+public class Animal {
+	protected int speed;
+	public void running() {
+		speed +=5;
+		System.out.println("동물이 뛰고 있어요. 현재 속도 : "+ speed);
+	}
+	public final void stop() {// override 금지 메소드 
+		speed = 0;
+		System.out.println("멈춤");		
+	}
+}
