0eeef75fb35efe84d2f0746f885e092b61e8c762
ssoom98 <breathin9898@gmail.com>
2024-09-05 10:13:20 +0900

    lec

---
+++ source/01_java/ch09_inheritance/src/com/lec/ex8_store/HeadQuaterStore.java
@@ -0,0 +1,25 @@
+package com.lec.ex8_store;
+//new HeadQuarterStore("본사") new Store1("1호점")
+public class HeadQuaterStore {
+	private String storeName;
+
+	public HeadQuaterStore(String storeName) {
+		super();
+		this.storeName = storeName;
+	}
+	public void kimchi() {
+		System.out.println("김치찌개 : 8,000원");
+	}
+	public void bude() {
+		System.out.println("부대지개 : 9,000원");
+	}
+	public void bibim() {
+		System.out.println("비빔밤 : 9,000원");
+	}
+	public void sunde()	{
+		System.out.println("순대국 : 8,000원");
+	}
+	public void gongi() {
+		System.out.println("공기밥 : 1,000원");
+	}
+}
