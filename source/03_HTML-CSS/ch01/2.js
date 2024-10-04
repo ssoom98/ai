63f5735ab7b15bb80573c94c5cdfb21fac905f97
ssoom98 <breathin9898@gmail.com>
2024-10-04 18:05:05 +0900

    form

---
+++ source/03_HTML-CSS/ch01/2.js
@@ -0,0 +1,9 @@
+﻿
+//2.js
+// 다른 이름으로 저장(utf-8인 코드로 인코딩)
+/* 동적인 부분(JaveScript)*/
+        name = prompt("이름은 >", "홍길동"); // 취소를 클릭하면 'null'리턴
+        if (name != 'null' && name != ""){
+            document.write(name + "님 반갑습니다<br>");
+
+        }
