b137a9f93b6c17628f18f26a7910b09468f20cd7
ssoom98 <breathin9898@gmail.com>
2024-10-15 18:00:15 +0900

    'JS'

---
+++ source/04_javaScript/ch02_기본문법/5_false들.js
@@ -0,0 +1,12 @@
+//false로 해석되는 값 : 0, '', undefined, NaN, null
+var i;
+console.log(Boolean(i));
+console.log(Boolean(0));
+console.log(Boolean(''));
+console.log(Boolean(NaN));
+console.log(Boolean(null));
+console.log();
+console.log(Boolean(-9999));
+
+console.log("빈스트링==false의 결과 : " +(""==false));
+console.log("빈스트링===false의 결과 : " +(""===false));
