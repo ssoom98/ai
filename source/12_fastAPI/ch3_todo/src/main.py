f73443035575778f82f79eac74b58e7d05f7f3bc
ssoom98 <breathin9898@gmail.com>
2025-01-21 12:15:07 +0900

    'todo_delete'

--- source/12_fastAPI/ch3_todo/src/main.py
+++ source/12_fastAPI/ch3_todo/src/main.py
@@ -1,6 +1,6 @@
 # src -> 소스 루트
 # pip install fastapi
-# pip install uvicorn
+# pip install uvicorn --no-cache-dir
 # pip install jinja2
 # pip install python_multipart (post사용)
 
@@ -8,6 +8,8 @@ from fastapi import FastAPI
 from fastapi import Request # template으로 갈 핸들러 함수 매개변수
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from starlette.responses import RedirectResponse
+
 from models import ToDoRequest
 from fastapi import Form # create(POST방식) 때 사용
 from fastapi import HTTPException
@@ -20,9 +22,6 @@ app.mount('/static',
           name='static')
 templates = Jinja2Templates(directory=os.path.join(BASE_DIR,'../templates'))
 
-@app.get('/')
-async def health_check_handler():
-    return {'status':'ok'}
 
 todo_data={
     1:{
@@ -48,10 +47,40 @@ todo_data={
 
 }
 
-
+@app.get('/')
+# async def health_check_handler():
+#     return {'status':'ok'}
 @app.get('/todos')
-async def get_todos_handler(order:str|None=None):
+@app.post('/todos')
+async def get_todos_handler(request:Request,order:str|None=None):
+
     todos = list(todo_data.values())
     if order and order.upper()=='DESC':
         todos = todos[::-1]
-    return todos
+    next_id = max(todo_data.keys())+1
+    return templates.TemplateResponse('todos.html', # todo 목록 , todo 입력 form
+                                      {'request':request,
+                                       'todos':todos,
+                                       'next_id':next_id,
+                                       'order':order.upper() if order else ''})
+# /todos/1
+@app.get('/todos/{todo_id}')
+async def get_todo_detail_handler(request:Request, todo_id:int):
+    todo= todo_data.get(todo_id,{})#todo_data[todo_id]#
+    return templates.TemplateResponse('todo.html',{'request':request,'todo':todo})
+
+@app.post('/create')
+async def create_todo_handler(todo:ToDoRequest=Form()):
+    #print('form태그로부터 입력된 todo :',todo)
+    todo_data[todo.id] =todo.dict()
+    #{'id':todo.id,'contents':todo.contents,'is_done':todo.is_done}
+    return RedirectResponse('/todos')
+
+@app.delete('/delete/{todo_id}')
+async def delete_todo_handler(todo_id:int):
+    #del todo_data[todo_id]
+    # 키가 없는 todo_id를 입력할 경우 None
+    todo = todo_data.pop(todo_id,None)
+    if todo:
+        return f'{todo_id}번 삭제 성공'
+    return f'{todo_id}는 등록되지 않는 todo여서 삭제 실패'
--- source/12_fastAPI/ch3_todo/src/test.py
+++ source/12_fastAPI/ch3_todo/src/test.py
@@ -5,3 +5,28 @@ BASE_DIR=os.path.dirname(filepath)
 print('현 폴더의 절대 경로:',BASE_DIR)
 static_folder = os.path.join(BASE_DIR,'../static')
 print('static 절대경로:', static_folder)
+
+todo_data={
+    1:{
+        'id':1,
+        'contents':'딥러닝',
+        'is_done':True
+    },
+    2:{
+        'id':2,
+        'contents':'fastAPI공부',
+        'is_done':False
+    },
+    3:{
+        'id':3,
+        'contents':'머신러닝 공부',
+        'is_done':False
+    },
+    4:{
+        'id':4,
+        'contents':'장고 공부',
+        'is_done':False
+    }
+
+}
+print(max(todo_data.keys())+1)
